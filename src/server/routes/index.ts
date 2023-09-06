4d69a9848beae78ad7e2480073c0ce8f343f0df2
LeonardoFertonaniM1 <leonardophotoshop1403@gmail.com>
2023-09-06 16:47:56 -0300

    feat: install http-status-codes

---
+++ src/server/routes/index.ts
@@ -0,0 +1,17 @@
+import { Router } from 'express';
+import { StatusCodes } from 'http-status-codes';
+
+const router = Router();
+
+
+router.get('/', (_, res) => {
+  return res.send('working');
+});
+
+router.post('/test/:id', (req, res) => {
+  return res.status( StatusCodes.ACCEPTED ).json(req.params); // .json return a json
+});
+
+
+
+export { router }; 
